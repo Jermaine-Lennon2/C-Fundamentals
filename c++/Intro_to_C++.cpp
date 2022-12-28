73e960f49d51de531184cd51c7e5344ad6e5b427
JermaineLennon <106980530+jermaine-lennon2@users.noreply.github.com>
2022-12-27 19:04:15 -0500

    ,

---
+++ c++/Intro_to_C++.cpp
@@ -0,0 +1,19 @@
+/*
+
+Introduction to C++ 
+
+C++ is a:
+    cross-platform language that can be used to create high-perfomace applications
+    developed by Bjarne Stroustrup as an extension to the C Language
+    give programmers a high level of contol over system resources and memory
+
+Why study C++?
+    C++ is one of the world's morst popular programming languages
+    C++ can be found in today's Operating systems, graphical user interfaces, and embedded systems
+    C++ is an object oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development cost
+    C++ is portable and can be used to develop applications that can be adapted to multiple platforms
+    
+Difference between C and C++ is that C++ supports classes and objects while C does not 
+
+
+*/
