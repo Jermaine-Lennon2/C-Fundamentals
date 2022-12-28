9a748e5e5223a0286d52eec6436fb98089cb5fab
JermaineLennon <106980530+jermaine-lennon2@users.noreply.github.com>
2022-12-27 19:40:39 -0500

    Create C++_Syntax.cpp

---
+++ c++/C++_Syntax.cpp
@@ -0,0 +1,14 @@
+// C++ Syntax
+
+#include <iostream>
+// this line is header file library that lets me work with input and output objects (cout). Header files add functionality to C++ programs 
+
+using namespace std;
+// i can use names for objects and variables from the standard libary 
+
+int main(){
+// This is a function that allows any code in the brackets to be executed
+    cout << "Hello World!";
+    // cout allows to output text
+    return 0;
+}          
