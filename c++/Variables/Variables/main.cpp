6e35d1d378b5ec781585b55114e1ba1456bda12b
JermaineLennon <106980530+jermaine-lennon2@users.noreply.github.com>
2022-12-30 15:02:25 -0500

    Variables

---
+++ c++/Variables/Variables/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Variables
+//
+//  Created by Jermaine T Lennon on 12/30/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
