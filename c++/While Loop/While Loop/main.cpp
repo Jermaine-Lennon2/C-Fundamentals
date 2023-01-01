b9d7a5365ad99ea6d22a8deed455ed30d0315fd1
JermaineLennon <106980530+jermaine-lennon2@users.noreply.github.com>
2023-01-01 12:00:28 -0500

    While loop

---
+++ c++/While Loop/While Loop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  While Loop
+//
+//  Created by Jermaine T Lennon on 1/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
