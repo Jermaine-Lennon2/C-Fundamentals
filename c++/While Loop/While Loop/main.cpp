64cc8ef5051f7527b85ad7c2e3d9152f8888b740
JermaineLennon <106980530+jermaine-lennon2@users.noreply.github.com>
2023-01-01 12:51:10 -0500

    .

--- c++/While Loop/While Loop/main.cpp
+++ c++/While Loop/While Loop/main.cpp
@@ -1,14 +1,29 @@
-//
-//  main.cpp
-//  While Loop
-//
-//  Created by Jermaine T Lennon on 1/1/23.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void whileLoop();
+
+int main (){
+    
+    whileLoop();
+    
+    
+    
     return 0;
+}
+
+void whileLoop(){
+    
+    int x;
+    
+    cout << "Enter a value for x: \n\n";
+    cin >> x;
+    
+    while(x < 25){
+        cout << x << "\n\n";
+        x++;
+    }
+    
+    
 }
